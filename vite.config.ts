82a8ad93beda3669e1f1bf416fbb139343717f9d
kudryaev.y <Kudryaev.Y.I@citilink.ru>
2026-02-09 20:26:08 +0400

    Добавил файл для докер контейнера и изменил конфиг vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replace(/\s*crossorigin/g, '')
+      },
+    },
+  ],
 })
